fb296f15283db1ec2e12e79eee06e669dea4030b
cuatr0-sh <cuatr0@icloud.com>
2025-04-19 19:20:13 +0200

    things

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -1,27 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 21:18:06 by root              #+#    #+#             */
-/*   Updated: 2025/04/18 22:10:26 by root             ###   ########.fr       */
+/*   Updated: 2025/04/18 22:42:15 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	unsigned char *d;
 	const unsigned char *s;
 	
 	d = (unsigned char *)dst;
 	s = (const unsigned char *)src;
-	while (n--)
+	if (d < s)
 	{
-		*d++ = *s++;
+		while (n--)
+		{
+			*d++ = *s++;
+		}
+	}
+	else
+	{
+		d = d + n;
+		s = s + n;
+		while (n--)
+		{
+			*--d = *--s;
+		}
 	}
 	return (dst);
 }
--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -1,27 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/17 21:18:06 by root              #+#    #+#             */
-/*   Updated: 2025/04/18 22:10:26 by root             ###   ########.fr       */
+/*   Created: 2025/04/18 22:51:36 by root              #+#    #+#             */
+/*   Updated: 2025/04/19 19:19:55 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+size_t	strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
 {
-	unsigned char *d;
-	const unsigned char *s;
 	
-	d = (unsigned char *)dst;
-	s = (const unsigned char *)src;
-	while (n--)
-	{
-		*d++ = *s++;
-	}
-	return (dst);
+}
+
+int 	main(int argc, char **argv)
+{
+	(void)argc;
+	printf("strlcat: %s", strlcat)
+
 }
