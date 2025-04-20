f27b32b4b5d780fb244f2529c41a6895a21c2a11
cuatr0-sh <cuatr0@icloud.com>
2025-04-20 22:37:57 +0200

    deleted a lot of bs

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 00:08:06 by root              #+#    #+#             */
-/*   Updated: 2025/04/11 02:25:54 by root             ###   ########.fr       */
+/*   Updated: 2025/04/20 17:17:56 by asoria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	if (!s)
-		return (NULL);
 	while (*s != '\0')
 	{
-		if (*s == (char)c)
+		if ((unsigned char)*s == (unsigned char)c)
 			return ((char *)s);
 		s++;
 	}
-	if ((char)c == '\0')
+	if ((unsigned char)c == '\0')
 		return ((char *)s);
 	return (NULL);
 }
