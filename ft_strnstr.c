1701db59abf7071eaa1d31a2f4819817418bb9a2
Arturo Soria <asoria@c2r16s4.42madrid.com>
2025-04-28 21:28:47 +0200

    ft_strnstr aadn ft_calloc (wip)

---
+++ ft_strnstr.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoria <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 20:12:07 by asoria            #+#    #+#             */
+/*   Updated: 2025/04/28 20:36:19 by asoria           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t  b;
+	size_t	l;
+
+	b = 0;
+	if (little[b] == '\0')
+		return ((char *)big);
+	while (big[b])
+	{
+		l = 0;
+		while (big[b + l] == little[l] && (b + l) < len)
+		{
+			if (big[b + l] == '\0' && little[l] == '\0')
+				return ((char *)big + b);
+			l++;
+		}
+		if (little[l] == '\0')
+			return ((char *)big + b);
+		b++;
+	}
+	return (0);
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	// printf("strnstr: %s\n", strnstr(argv[1], argv[2], atoi(argv[3])));
+	 printf("ft_strnstr: %s\n", ft_strnstr(argv[1], argv[2], atoi(argv[3])));
+	return (0);
+}*/
