aebfcb807f1322cb3cad4a8aee7b8fb31ca3ced6
cuatr0-sh <cuatr0@icloud.com>
2025-05-12 21:19:29 +0200

    casted c to unsigned char for protection and to stop francinette from whining

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 19:44:44 by root              #+#    #+#             */
-/*   Updated: 2025/05/06 03:40:55 by root             ###   ########.fr       */
+/*   Updated: 2025/05/12 21:17:53 by asoria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,11 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 	{
-		if (*s == c)
+		if (*s == (unsigned char)c)
 			return ((char *)s);
 		s++;
 	}
-	if (c == '\0')
+	if ((unsigned char)c == '\0')
 		return ((char *)s);
 	return ((void *)0);
 }
-/*
-#include <string.h>
-#include <stdio.h>
-
-int	main(int argc, char **argv)
-{
-	(void)argc;
-	printf("strchr: %s\n", strchr(argv[1], argv[2][0]));
-	printf("ft_strchr: %s\n", ft_strchr(argv[1], argv[2][0]));
-}*/
