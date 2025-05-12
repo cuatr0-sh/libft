df469e0b4090618e30523b79dbb52a87863dcc07
cuatr0-sh <cuatr0@icloud.com>
2025-05-12 23:59:46 +0200

    unprotected parameter input for it to segfault if null (original libc does), and protected malloc return

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: asoria <asoria@student.42madrid.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 18:08:39 by asoria            #+#    #+#             */
-/*   Updated: 2025/05/06 02:03:06 by root             ###   ########.fr       */
+/*   Updated: 2025/05/12 23:58:29 by asoria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ char	*ft_strdup(const char *s)
 	char	*str;
 	size_t	len;
 
-	if (!s)
-		return ((void *)0);
 	len = ft_strlen(s) + 1;
 	str = malloc(len);
+	if (!str)
+		return ((void *)0);
 	ft_memcpy(str, s, len);
 	return (str);
 }
