54c638cb36c53b4a0db63298704583a839a31a50
cuatr0-sh <cuatr0@icloud.com>
2025-05-13 04:31:35 +0200

    ft_substr solved segv in massive argv2

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: asoria <asoria@student.42madrid.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 00:44:01 by asoria            #+#    #+#             */
-/*   Updated: 2025/05/06 03:52:26 by root             ###   ########.fr       */
+/*   Updated: 2025/05/13 00:20:19 by asoria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*ret;
+	size_t	s_len;
 
 	if (!s)
-		return (0);
-	if (ft_strlen(s) < start)
-		len = 0;
-	if (ft_strlen(s + start) < len)
-		len = ft_strlen(s + start);
-	ret = malloc(sizeof(char) * (len + 1));
+		return ((void *)0);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	ret = malloc(len + 1);
 	if (!ret)
-		return (0);
+		return ((void *)0);
 	ft_strlcpy(ret, s + start, len + 1);
 	return (ret);
 }
