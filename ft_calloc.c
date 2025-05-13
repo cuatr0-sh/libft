164449f43dc0c9662c56ef88233afbf20d73549c
Arturo Soria <asoria@c2r12s6.42madrid.com>
2025-05-13 21:34:53 +0200

    overflow protection and norm check

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: asoria <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 20:37:02 by asoria            #+#    #+#             */
-/*   Updated: 2025/05/05 17:58:24 by asoria           ###   ########.fr       */
+/*   Updated: 2025/05/13 21:31:40 by asoria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
+	size_t	bytes;
 	void	*res;
 
+	bytes = nmemb * size;
+	if (size && ((bytes / size) != nmemb))
+		return ((void *)0);
 	res = malloc(size * nmemb);
 	if (!res)
 		return ((void *)0);
