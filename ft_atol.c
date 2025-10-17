7e687e1c538a56bea634f45f06da1d5b63cd9537
cuatr0-sh <cuatr0@icloud.com>
2025-10-17 07:49:54 +0200

    norm ft_atol.c

--- ft_atol.c
+++ ft_atol.c
@@ -1,16 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoria <asoria@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 07:47:30 by asoria            #+#    #+#             */
+/*   Updated: 2025/10/17 07:49:04 by asoria           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stddef.h>
 
 long	ft_atol(const char *str)
 {
-	int		i = 0;
-	int		neg = 1;
-	long	res = 0;
+	int		i;
+	int		neg;
+	long	res;
 
+	i = 0;
+	neg = 1;
+	res = 0;
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
 		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
 	if (str[i] == '-')
-		neg = -1, i++;
+	{
+		neg = -1;
+		i++;
+	}
 	else if (str[i] == '+')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
