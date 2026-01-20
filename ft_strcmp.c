8863570dcd8f569a975206a2e4539c5715e3f3af
cuatr0-sh <cuatr0@icloud.com>
2026-01-20 05:20:03 +0100

    fixed check of s2 and changed name because ft_ was missing

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,18 +6,18 @@
 /*   By: asoria <asoria@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 00:07:21 by asoria            #+#    #+#             */
-/*   Updated: 2026/01/20 00:31:48 by asoria           ###   ########.fr       */
+/*   Updated: 2026/01/20 05:19:15 by asoria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	strcmp(const char *s1, const char *s2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s1[i] && s1[i] == s2[i])
+	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
